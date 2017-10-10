b33291cbdb0345d5a06569558f2a28eced2ef704
Putyra, Rob <rpputyr.ilstu.edu>
2017-10-09 19:58:53 -0500

    Add Column Heading Index Locator

--- src/FileOrganizer.java
+++ src/FileOrganizer.java
@@ -14,6 +14,7 @@ public class FileOrganizer {
     
     String classNumber, semester, year, firstName, lastName, grade;
     String classData[] = new String[3];
+    boolean fullNameUsed = false;
 
     
     public void readFile(String file) {
@@ -26,7 +27,8 @@ public class FileOrganizer {
   	  	number = fileName.indexOf("-");
   	  	semester = fileName.substring(0, number);
   	  	fileName = fileName.substring(number+1);
-  	  	year = fileName;
+  	  	number = fileName.indexOf(".");
+  	  	year = fileName.substring(0, number);
   	  	
   	  	classData[0] = classNumber;
   	  	classData[1] = semester;
@@ -35,8 +37,38 @@ public class FileOrganizer {
   	  	array.add(classData);
   	  	
     	 try {
-    		 file.indexOf("-");
     	        br = new BufferedReader(new FileReader(file));
+    	        
+    	        //The first row holds the column headings for the file.
+    	        //These next few lines determine which headings are which to help 
+    	        //identify lines in the rest of the file.
+    	        
+    	        int idLoc, oneNameLoc, twoNameLoc, fullNameLoc, gradeLoc;
+    	        line = br.readLine();
+    	        String[] firstLine = line.split(cvsSplitBy);
+    	        
+    	        for(int i = 0; i<firstLine.length; i++) {
+    	        	if (firstLine[i].toLowerCase().indexOf("id") != -1) {
+    	        		idLoc = i;
+    	        		return;
+    	        	}
+    	        	
+    	        	else if(firstLine[i].toLowerCase().indexOf("name") != -1) {
+    	        		if(firstLine[i].toLowerCase().indexOf("first") != -1) {
+    	        			oneNameLoc = i;
+    	        		}
+    	        		else if (firstLine[i].toLowerCase().indexOf("last") != -1) {
+    	        			twoNameLoc = i;
+    	        		}
+    	        		else {
+    	        			fullNameLoc = i;
+    	        			fullNameUsed = true;
+    	        		}
+    	        	}
+    	        }
+    	        
+    	        
+    	        
     	        while ((line = br.readLine()) != null) {
     	        	 String[] lineElements = line.split(cvsSplitBy);
     	        	 for(int i=0; i<lineElements.length; i++) {

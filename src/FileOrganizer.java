55420eea24c775521da86811dedcdf53db0d26af
Putyra, Rob <rpputyr.ilstu.edu>
2017-10-09 20:56:25 -0500

    Add tests for printed arrays

--- src/FileOrganizer.java
+++ src/FileOrganizer.java
@@ -10,7 +10,7 @@ public class FileOrganizer {
     BufferedReader br = null;
     String line = "";
     String cvsSplitBy = ",";
-    ArrayList<String[]> array = new ArrayList();
+    ArrayList<String[]> array = new ArrayList<String[]>();
     
     String classNumber, semester, year, firstName, lastName, grade;
     String classData[] = new String[3];
@@ -36,6 +36,8 @@ public class FileOrganizer {
   	  	
   	  	array.add(classData);
   	  	
+  	  	classData = null;
+  	  	
     	 try {
     	        br = new BufferedReader(new FileReader(file));
     	        
@@ -43,14 +45,15 @@ public class FileOrganizer {
     	        //These next few lines determine which headings are which to help 
     	        //identify lines in the rest of the file.
     	        
-    	        int idLoc, oneNameLoc, twoNameLoc, fullNameLoc, gradeLoc;
+    	        int idLoc = -1, oneNameLoc = -1, twoNameLoc = -1, fullNameLoc = -1, gradeLoc = -1;
     	        line = br.readLine();
     	        String[] firstLine = line.split(cvsSplitBy);
+    	        int rowLength = firstLine.length;
+    	        classData = new String [rowLength];
     	        
     	        for(int i = 0; i<firstLine.length; i++) {
     	        	if (firstLine[i].toLowerCase().indexOf("id") != -1) {
     	        		idLoc = i;
-    	        		return;
     	        	}
     	        	
     	        	else if(firstLine[i].toLowerCase().indexOf("name") != -1) {
@@ -65,16 +68,53 @@ public class FileOrganizer {
     	        			fullNameUsed = true;
     	        		}
     	        	}
+    	        	else if (firstLine[i].toLowerCase().indexOf("grade") != -1) {
+    	        		gradeLoc = i;
+    	        	}
     	        }
     	        
-    	        
+    	        int startingIndex = 4;
+    	        if (fullNameUsed == true) {
+    	        	if (fullNameLoc < gradeLoc) {
+    	        		gradeLoc++;
+    	        	}
+    	        	if (fullNameLoc < idLoc) {
+    	        		idLoc++;
+    	        	}
+    	        }
     	        
     	        while ((line = br.readLine()) != null) {
     	        	 String[] lineElements = line.split(cvsSplitBy);
     	        	 for(int i=0; i<lineElements.length; i++) {
-    	        		 System.out.println(lineElements[i]);
+    	        		 if (i == idLoc) {
+    	        			 classData[0] = lineElements[i]; 
+    	        		 }
+    	        		 else if (i==oneNameLoc) {
+    	        			 classData[1] = lineElements[i];
+    	        		 }
+    	        		 else if(i==twoNameLoc) {
+    	        			 classData[2] = lineElements[i];
+    	        		 }
+    	        		 else if(i==fullNameLoc) {
+    	        			 classData[1] = lineElements[i];
+    	        		 }
+    	        		 else if(i==(fullNameLoc+1)) {
+    	        			 classData[2] = lineElements[i];
+    	        		 }
+    	        		 else if (i == gradeLoc) {
+    	        			 classData[3] = lineElements[i];
+    	        		 }
+    	        		 else {
+    	        			 classData[startingIndex] = lineElements[i];
+    	        			 startingIndex++;
+    	        		 }
     	        	 }
-//    	        	 System.out.println(country);
+    	        	 
+    	        	 array.add(classData);
+    	        	 classData = null;
+    	        	 classData = new String[rowLength];
+    	        	 
+    	        	 startingIndex = 4;
     	        }
 
     	    } catch (FileNotFoundException e) {
@@ -90,6 +130,19 @@ public class FileOrganizer {
     	            }
     	        }
     	    }
+    	
+    }
+    
+    public void printArray() {
+    	int size = array.size();
+    	
+    	for (int i = 1; i < size; i++) {
+    		String[] example = array.get(i);
+        	for(int j = 0; j<example.length; j++) {
+        		System.out.print(example[j] + " ");
+        	}
+        	System.out.println();
+    	}
     	
     }
 }

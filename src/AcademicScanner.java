9bafa7b4a5dddc7bbb5657375c06b70e0a69a0ad
finem50 <finem50@live.com>
2017-10-13 00:13:02 -0500

    Adding getters and setters

--- src/AcademicScanner.java
+++ src/AcademicScanner.java
@@ -1,5 +1,7 @@
 public class AcademicScanner {
+
     public static void main(String[] args) {
-        System.out.println("Branch: Max");
+        FileOrganizer csvOrganizer = new FileOrganizer();
+
     }
 }
